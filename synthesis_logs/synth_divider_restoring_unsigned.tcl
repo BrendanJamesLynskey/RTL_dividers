6b17883078f0a2d8b3e05ecb800f017a19d98238
Brendan Lynskey <brendanlynskey@googlemail.com>
2026-03-19 21:46:01 +0000

    Add Vivado synthesis results for Artix-7 (xc7a35tcpg236-1)

---
+++ synthesis_logs/synth_divider_restoring_unsigned.tcl
@@ -0,0 +1,15 @@
+set_param general.maxThreads 4
+create_project -in_memory -part xc7a35tcpg236-1
+
+read_verilog -sv /home/brendan/synthesis_workspace/Integer_dividers/divider_restoring_unsigned.sv
+
+set xdc_file "/home/brendan/synthesis_workspace/Integer_dividers/synthesis_logs/clock_divider_restoring_unsigned.xdc"
+set fp [open $xdc_file w]
+puts $fp "create_clock -period 10.000 -name CLK \[get_ports CLK\]"
+close $fp
+read_xdc $xdc_file
+
+synth_design -top divider_restoring_unsigned -part xc7a35tcpg236-1
+
+report_utilization -file /home/brendan/synthesis_workspace/Integer_dividers/synthesis_logs/utilization_divider_restoring_unsigned.rpt
+report_timing_summary -file /home/brendan/synthesis_workspace/Integer_dividers/synthesis_logs/timing_divider_restoring_unsigned.rpt
